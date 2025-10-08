d79834dafdc3cc57c66cdddccdfd6ac1786173f0
Cesar Smaniotto Júnior <cesarsjb@gmail.com>
2025-10-08 12:19:58 -0300

    fix footer

--- components/ui/Footer/Footer.jsx
+++ components/ui/Footer/Footer.jsx
@@ -19,10 +19,10 @@ const Footer = () => {
                 {/* Logo e Empresa */}
                 <div className="flex flex-col space-y-4">
                     <div className="flex items-center">
-                        <Image 
-                            src="/logo-qv.png" 
-                            alt="QV Business Solution" 
-                            width={120} 
+                        <Image
+                            src="/logo-qv.png"
+                            alt="QV Business Solution"
+                            width={120}
                             height={40}
                             className="h-10 w-auto"
                         />
@@ -41,11 +41,11 @@ const Footer = () => {
                         {
                             footerNavs.map((item, idx) => (
                                 <li key={idx}>
-                                    <a 
+                                    <a
                                         href={item.href}
                                         className="text-sm hover:text-primary-800 transition-colors duration-200"
                                     >
-                                        { item.name }
+                                        {item.name}
                                     </a>
                                 </li>
                             ))
@@ -59,8 +59,8 @@ const Footer = () => {
                         Conecte-se
                     </h3>
                     <ul className="flex items-center space-x-4">
-                        <li className="w-10 h-10 border border-gray-300 rounded-full flex items-center justify-center hover:border-primary-600 hover:bg-primary-50 transition-all duration-200">
-                            <a href="https://www.instagram.com/qvconsultoriafinanceira" aria-label="Instagram">
+                        <a href="https://www.instagram.com/qvconsultoriafinanceira" aria-label="Instagram">
+                            <li className="w-10 h-10 border border-gray-300 rounded-full flex items-center justify-center hover:border-primary-600 hover:bg-primary-50 transition-all duration-200">
                                 <svg className="svg-icon w-6 h-6 text-gray-600 hover:text-primary-600" fill="none" viewBox="0 0 32 32" xmlns="http://www.w3.org/2000/svg">
                                     <g id="SVGRepo_bgCarrier" strokeWidth="0"></g>
                                     <g id="SVGRepo_tracerCarrier" strokeLinecap="round" strokeLinejoin="round"></g>
@@ -69,8 +69,8 @@ const Footer = () => {
                                         <path d="M16 10.312c-3.138 0-5.688 2.551-5.688 5.688s2.551 5.688 5.688 5.688 5.688-2.551 5.688-5.688-2.55-5.688-5.688-5.688zm0 9.163a3.475 3.475 0 1 1-.001-6.95 3.475 3.475 0 0 1 .001 6.95zM21.7 8.991a1.363 1.363 0 1 1-1.364 1.364c0-.752.51-1.364 1.364-1.364z"></path>
                                     </g>
                                 </svg>
-                            </a>
-                        </li>
+                            </li>
+                        </a>
                     </ul>
                 </div>
             </div>
