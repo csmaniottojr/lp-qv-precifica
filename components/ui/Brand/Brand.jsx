557bd396626a1e55138e2f83a2d31dac1d6e65d9
Cesar Smaniotto Júnior <cesarsjb@gmail.com>
2025-10-08 11:37:21 -0300

    Melhora layout do footer e atualiza branding para QV Business Solution

--- components/ui/Brand/Brand.jsx
+++ components/ui/Brand/Brand.jsx
@@ -2,8 +2,8 @@ import Image from "next/image"
 
 const Brand = ({ ...props }) => (
     <Image
-        src="/mailgo.svg"
-        alt="Mailgo logo"
+        src="/logo-qv.png"
+        alt="QV Precifica"
         {...props}
         width={110}
         height={50}
