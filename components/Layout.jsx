814c819647a2b0b2a8b54301a329c5e293496b24
Cesar Smaniotto Júnior <cesarsjb@gmail.com>
2025-10-09 23:25:17 -0300

    fix title

--- components/Layout.jsx
+++ components/Layout.jsx
@@ -6,8 +6,8 @@ const Layout = ({ children }) => {
     return (
         <>
             <Head>
-                <title>Mailgo</title>
-                <meta name='description' content="Gain control of your business's growth with Mailgo's comprehensive marketing, automation, and email marketing platform." />
+                <title>QV Precifica</title>
+                <meta name='description' content="Aumente seu lucro com precificação baseada em dados reais" />
                 <meta name='viewport' content='width=device-width, initial-scale=1' />
                 <link rel='icon' href='/favicon.ico' />
             </Head>
