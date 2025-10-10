1bb0d0774bafdeb5494a4755440fe44936456b46
Cesar Smaniotto Júnior <cesarsjb@gmail.com>
2025-10-09 21:50:48 -0300

    add tag manager

--- pages/_document.js
+++ pages/_document.js
@@ -1,8 +1,10 @@
 import { Html, Head, Main, NextScript } from "next/document";
+import { GoogleTagManager } from '@next/third-parties/google'
 
 export default function Document() {
   return (
     <Html lang='en'>
+      <GoogleTagManager gtmId="GTM-5BW658ZD" />
       <Head />
       <body className='bg-gray-900'>
         <Main />
